7e2ede2029a1c7505e21f7ea8144ca7cb586ad62
VitaliMay <mvt30@mail.ru>
2023-09-26 18:32:43 +0300

    init: start task Image-Galery

---
+++ image-galery/js/index.js
@@ -0,0 +1,59 @@
+const score = `
+Проверка подключения.
+Всё в порядке)
+
+`
+
+console.log(score)
+
+// подключаю плейлист
+import arrSong from "./playList.js";
+
+
+const wrapper = document.querySelector('.wrapper')
+const playerContainer = document.querySelector('.cover-content')
+
+
+//let currentSong = arrSong[songNum].src
+//audio.src = currentSong;
+
+// audio.addEventListener('loadeddata', function() {
+//   timeDuration.innerHTML = `${timeFromSec(audio.duration)}`;
+//   titleSinger.innerHTML = `${arrSong[songNum].group}`;
+//   titleSong.innerHTML = `${arrSong[songNum].name}`;
+//   wrapper.style.backgroundImage =`url(${arrSong[songNum].cover})`
+//   playerContainer.style.backgroundImage =`url(${arrSong[songNum].cover})`
+
+// });
+
+
+
+
+
+//***Для кнопок*************************************
+// Коды HTML
+
+// &#9658;	►	Треугольная стрелка вправо
+// &#9668;	◄	Треугольная стрелка влево
+
+// ‖
+// &#8214;
+// \2016
+// U+2016
+// &Vert;
+// Двойная вертикальная линия
+
+// ⊲
+// &#8882;
+// \22B2
+// U+22B2
+// &vltri;
+// Нормальная подгруппа
+// ⊳
+// &#8883;
+// \22B3
+// U+22B3
+// &vrtri;
+// Содержит как нормальную подгруппу
+
+
