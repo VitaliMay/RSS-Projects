bc6f362773e5064eb90480fe3e91e6c2f255df59
VitaliMay <mvt30@mail.ru>
2024-01-14 21:34:58 +0300

    docs: add riddle

--- hangman/index-copy.js
+++ hangman/index-copy.js
@@ -1,4 +1,14 @@
 
+/**************************************************************** */
+  /**************************************************************** */
+  // подключаю модуль
+  // import data from "./products";
+  console.log(data)
+  /**************************************************************** */
+  /**************************************************************** */
+
+
+
 let abc = [
   [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, '', ' ', 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 92, 91, 93, '', '', 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, '', '', 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, '', '', '', '', '', ' ', '', '', '', '', ''],
   [1105, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 1081, 1094, 1091, 1082, 1077, 1085, 1075, 1096, 1097, 1079, 1093, 1098, 1092, 1099, 1074, 1072, 1087, 1088, 1086, 1083, 1076, 1078, 1101, 92, 1103, 1095, 1089, 1084, 1080, 1090, 1100, 1073, 1102, 46]
@@ -36,16 +46,29 @@ function start(){
   /**** Рисую картинку ******************************************** */
 
   const hangImgWrapper = document.querySelector('.hangImg');
-  // const div = document.createElement('div');
+  const div = document.createElement('div');
+  div.classList.add(`hangImgMen`);
+  div.classList.add(`hangImgMen_position`)
+  hangImgWrapper.appendChild(div);
+
+  const hangImgMen = document.querySelector('.hangImgMen');
   for (let i = 1; i <= 6; i += 1) {
     const div = document.createElement('div');
-    div.classList.add(`hangImg__item`);
-    div.classList.add(`hangImg__item_${i}`);
-    hangImgWrapper.appendChild(div);
-    console.log(i)
+    div.classList.add(`hangImgMen__item`);
+    div.classList.add(`hangImgMen__item_${i}`);
+    hangImgMen.appendChild(div);
   }
+  // const hangImgWrapper = document.querySelector('.hangImg');
+  
+  // for (let i = 1; i <= 6; i += 1) {
+  //   const div = document.createElement('div');
+  //   div.classList.add(`hangImg__item`);
+  //   div.classList.add(`hangImg__item_${i}`);
+  //   hangImgWrapper.appendChild(div);
+  //   console.log(i)
+  // }
 
-  /**************************************************************** */
+  /************************************************************************** */
 
   let init = '';
   for (let i = 0; i < codeKeyboard.length; i++){
