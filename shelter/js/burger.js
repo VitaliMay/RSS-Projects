908df12bc4ec118e746c65c1431d46f33e58a352
VitaliMay <mvt30@mail.ru>
2024-10-17 16:59:03 +0300

    refactor: delete folder after resolve conflict

--- shelter/js/burger.js
+++
@@ -1,9 +0,0 @@
-export const checkBurger = 'Проверка импорта из бургер. Ура. Заработало'
-
-// import { checkIndex } from "../index.js"
-
-
-// console.log(checkIndex)
-
-// const sliderBtnLeft = document.querySelector('.slider__btn--order-01')
-// export { sliderBtnLeft }
