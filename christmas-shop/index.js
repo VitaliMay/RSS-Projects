ed3bd774f99d21ecbfba09836c6254851954e1e2
VitaliMay <mvt30@mail.ru>
2024-10-30 17:04:21 +0300

    feat: add footer base layout

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -1,1 +1,16 @@
-console.log('Merry Christmas')
+
+
+const score = `
+Merry Christmas!
+`
+
+function signatureScore () {
+  console.log(score)
+  console.log(
+    '%c   /\\     /\\  \n  //\\\\___//\\\\\n  \\\\       //\n   \\\\o _ \o//    \n    \\ /=\\ /  \n       \' ',
+    'color: orange; font-weight: bold; font-size: 12px;'
+  );
+}
+
+
+signatureScore ()
