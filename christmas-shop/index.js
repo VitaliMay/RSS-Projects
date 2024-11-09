a4380e3b329a65e705b277d8dea1185abff6efaf
VitaliMay <mvt30@mail.ru>
2024-11-09 19:12:54 +0300

    fix: correct for task part-1 - fixed Layout

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -1,7 +1,7 @@
 
 import { timerInit } from "./js/timer.js";
 import { signatureScore } from "./js/score.js";
-import { testCreatCard, dataIndexArrAll, dataIndexPP, dataIndexBestPP, giftCardContainer, bestGiftCardContainer } from "./js/card.js";
+import { testCreatCard, dataIndexArrAll, dataIndexArrAllShuffle, dataIndexPP, dataIndexBestPP, giftCardContainer, bestGiftCardContainer } from "./js/card.js";
 
 // timerInit ()
 
@@ -10,10 +10,11 @@ import { testCreatCard, dataIndexArrAll, dataIndexPP, dataIndexBestPP, giftCardC
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  timerInit ()
-  // testCreatCard(dataIndexPP, giftCardContainer)
-  testCreatCard(dataIndexArrAll, giftCardContainer)
-  // testCreatCard(dataIndexBestPP, bestGiftCardContainer)
+  // timerInit ()
+  testCreatCard(dataIndexPP, giftCardContainer)
+  // testCreatCard(dataIndexArrAll, giftCardContainer)
+  // testCreatCard(dataIndexArrAllShuffle, giftCardContainer)
+
   testCreatCard(dataIndexBestPP, bestGiftCardContainer)
 });
 
