e02e644d1b7d1c77aed93259355c45f6ae8e87cd
VitaliMay <mvt30@mail.ru>
2025-01-08 20:37:39 +0300

    feat: add basic interface layout

--- simon-says/js/variables.js
+++ simon-says/js/variables.js
@@ -1,4 +1,10 @@
 
 const body = document.querySelector('body')
+const wrapper = document.querySelector('.wrapper')
+const header = document.querySelector('.header')
+const main = document.querySelector('.main')
+const keyboardNum = document.querySelector('.keyboard-num')
+const keyboardLetter = document.querySelector('.keyboard-letter')
+// console.log(keyboardNum)
 
-export { body }
+export { body, wrapper, header, main, keyboardNum, keyboardLetter }
