70408cc37f1dcdd1050c9860ebd0e4454898645c
VitaliMay <mvt30@mail.ru>
2025-10-12 21:51:37 +0300

    dosc: delete unnecessary folders

--- simon-says/app.js
+++
@@ -1,9 +0,0 @@
-import { signatureScore } from "./js/score.js";
-import { body, header, keyboardNum } from "./js/variables.js";
-import { createEl } from "./js/elementUtils.js";
-import { createKeyboard } from "./js/keyboard.js";
-import { changeLevel } from "./js/level.js";
-import { createSequence } from "./js/game-logic.js";
-
-
-signatureScore ()
